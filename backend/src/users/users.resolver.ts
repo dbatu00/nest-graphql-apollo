08ddbb086db98dec6f552897bb698609076de834
Dbatu00 <denizbatu@sabanciuniv.edu>
2025-09-12 22:34:19 +0300

    add comment on getUser

--- backend/src/users/users.resolver.ts
+++ backend/src/users/users.resolver.ts
@@ -11,6 +11,7 @@ export class UsersResolver {
     return this.usersService.findAll();
   }
 
+  //graphql will default to float if type is not cast
   @Query(() => User, { nullable: true })
   getUser(@Args('id', { type: () => Int }) id: number) {
     return this.usersService.findOne(id);
