5f7ad94e641ae7c66a719ebab3e048aa6db6fbce
Dbatu00 <denizbatu@sabanciuniv.edu>
2025-09-15 17:40:09 +0300

    add delete user input

---
+++ backend/src/users/delete-user.input.ts
@@ -0,0 +1,13 @@
+import { InputType, Field, Int } from '@nestjs/graphql';
+
+@InputType()
+export class DeleteUserInput {
+  @Field(() => Int)
+  id: number;
+
+  @Field(() => String)
+  name: string;
+
+  @Field(() => Boolean) // graphQL type function
+  force: boolean; //typescript type annotation
+}
