c576e48d8f02803d808ac12c3bd88270b299c585
Dbatu00 <denizbatu@sabanciuniv.edu>
2025-09-17 00:33:43 +0300

    change addUserInput force field to be non nullable and default false

--- backend/src/users/add-user.input.ts
+++ backend/src/users/add-user.input.ts
@@ -5,6 +5,6 @@ export class AddUserInput {
   @Field(() => String)
   name!: string;
 
-  @Field(() => Boolean, { nullable: true }) // graphQL type function
-  force?: boolean; //typescript type annotation
+  @Field(() => Boolean, { defaultValue: false }) // GraphQL default
+  force: boolean = false; // TypeScript default
 }
