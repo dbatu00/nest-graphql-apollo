06d1c8718851141f708585a1c0161fe1833cb3fb
Dbatu00 <denizbatu@sabanciuniv.edu>
2025-12-03 22:51:18 +0300

    update jest config

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,11 +1,22 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const tsconfig = require("./tsconfig.json");
+
 module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
+
   transformIgnorePatterns: [
-    'node_modules/(?!(expo|expo-modules-core|@expo|@unimodules|react-native|@react-native|@react-navigation)/)'
+    'node_modules/(?!(expo|expo-modules-core|@expo|@unimodules|react-native|@react-native|@react-navigation)/)',
   ],
+
+  moduleNameMapper: pathsToModuleNameMapper(
+    tsconfig.compilerOptions.paths,
+    { prefix: '<rootDir>/' }
+  ),
+
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
