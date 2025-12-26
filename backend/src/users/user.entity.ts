2c8700673919c4ab1c02f81ba5b9a319e8745c7a
Dbatu00 <denizbatu@sabanciuniv.edu>
2025-12-26 20:54:41 +0300

    change db table name

--- backend/src/users/user.entity.ts
+++ backend/src/users/user.entity.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
-@Entity()
+@Entity('users')
 export class User {
   @Field(() => Int)
   @PrimaryGeneratedColumn()
