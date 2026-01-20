22c78d831b9d32bec8a13d59e8fd4ce257a7a928
Dbatu00 <denizbatu@sabanciuniv.edu>
2026-01-20 16:58:53 +0300

    silence misc warnings

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,6 +1,13 @@
 import { useEffect } from "react";
 import { router } from "expo-router";
 import { useAuth } from "../hooks/useAuth";
+import { LogBox } from 'react-native';
+
+// Ignore specific warnings
+LogBox.ignoreLogs([
+  'shadow* style props are deprecated',  // boxShadow warning
+  'props.pointerEvents is deprecated',   // pointerEvents warning
+]);
 
 export default function Index() {
   const { user, loading } = useAuth();
